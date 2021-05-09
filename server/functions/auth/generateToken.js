e2590760d97bd3c8dff19cc45e359829eec15a2f
yutashimizu0612 <s08388yskh@gmail.com>
2021-05-09 11:54:48 +0900

    一旦削除

--- server/functions/auth/generateToken.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-
-exports.generateToken = async (payload, secret, period) => {
-  const token = jwt.sign(payload, secret, {
-    expiresIn: period,
-  });
-  return token;
-};
--- server/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-const expressJwt = require('express-jwt');
-require('dotenv').config();
-
-exports.requireLogin = expressJwt({
-  secret: process.env.JWT_SECRET,
-  algorithms: ['HS256'],
-});
